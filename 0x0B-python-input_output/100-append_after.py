c536edc1af204b92ea619ed2c11c8a929c042fad
Obayopo Ridwan <obayoporidwantemi@gmail.com>
2023-01-10 20:44:36 -0800

    python input_output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+"""This module defines a text file insertion function"""
 
 
-class Student:
-    """Represent a student."""
-
-    def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """Gets a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
-
-    def reload_from_json(self, json):
-        """Replaces all attributes of the Student
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts text after each line containing a given string in a file
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
