624e2f29c49c8322a5c83785d65d3b52b779ef78
Obayopo Ridwan <obayoporidwantemi@gmail.com>
2023-04-04 15:43:03 -0700

    python_network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 #Write a Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
-
-url = "https://alx-intranet.hbtn.io/status"
-
-with urllib.request.urlopen(url) as response:
-    html = response.read()
 if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+    html = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
