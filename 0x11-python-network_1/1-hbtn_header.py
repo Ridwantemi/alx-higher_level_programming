4779d3732cc0422599ddcbd0f46d11d485a1b83a
Ridwantemi <obayoporidwantemi@gmail.com>
2023-04-04 21:45:25 -0700

    python_networking

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,21 +2,22 @@
 # a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response
 import urllib.request
 import sys
+ if __name__ == "__main__":
 
-# Check if URL argument is provided
-if len(sys.argv) < 2:
-    print("Error: URL argument is required.")
-    sys.exit(1)
+    # Check if URL argument is provided
+    if len(sys.argv) < 2:
+        print("Error: URL argument is required.")
+        sys.exit(1)
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.getheader('X-Request-Id')
-        if x_request_id:
+    try:
+        with urllib.request.urlopen(url) as response:
+            x_request_id = response.getheader('X-Request-Id')
+            if x_request_id:
             print("{}".format(x_request_id))
-        else:
+            else:
             print("X-Request-Id not found in the response header.")
-except urllib.error.URLError as e:
-    print("Error fetching URL: {}".format(e))
+    except urllib.error.URLError as e:
+        print("Error fetching URL: {}".format(e))
 
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,21 +3,21 @@
 import urllib.request
 import urllib.error
 import sys
+if __name__ == "__main__":)
+    # Check if URL argument is provided
+    if len(sys.argv) < 2:
+        print("Error: URL argument is required.")
+        sys.exit(1)
 
-# Check if URL argument is provided
-if len(sys.argv) < 2:
-    print("Error: URL argument is required.")
-    sys.exit(1)
+    url = sys.argv[1]
 
-url = sys.argv[1]
-
-try:
-    with urllib.request.urlopen(url) as response:
-        html = response.read().decode('utf-8')
-        print("Response body:")
-        print(html)
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
-except urllib.error.URLError as e:
-    print("Error fetching URL: {}".format(e))
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode('utf-8')
+            print("Response body:")
+            print(html)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+    except urllib.error.URLError as e:
+        print("Error fetching URL: {}".format(e))
 
